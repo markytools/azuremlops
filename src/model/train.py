db845c481783dfeeed3d004c88730d65d06b0774
Mark Vincent Ty <markytools@gmail.com>
2024-01-31 18:43:10 +0800

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -7,12 +7,14 @@ import os
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import mlflow
 
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -20,8 +22,9 @@ def main(args):
     # split data
     X_train, X_test, y_train, y_test = split_data(df)
 
-    # train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    with mlflow.start_run():
+        # train model
+        train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
 
 def get_csvs_df(path):
@@ -32,14 +35,15 @@ def get_csvs_df(path):
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
-
 # TO DO: add function to split data
-
+def split_data(df):
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
     LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
-
 
 def parse_args():
     # setup arg parser
